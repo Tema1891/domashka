8a9404c600ee33b51a7d4a05e206d3707ae97a7e
Tema1891 <122228074+Tema1891@users.noreply.github.com>
2023-04-09 00:52:24 +0300

    Add files via upload

---
+++ домашка 13.8.19.py
@@ -0,0 +1,21 @@
+tickets = int(input("При покупке от 4-х билетов действует скидка 10% от стоимости заказа!\n"
+                    'Введите количество желаемых билетов : '))
+if tickets > 3:
+    print('Вам доступна скидка 10% ')
+ages = list(map(int, input('Укажите возраст посетителей через пробел, согласно количеству билетов: ').split()))
+if len(ages) != tickets:
+    print('Вы не указали возраст всех участников. Укажите возраст всех участников!')
+    ages = list(map(int, input('Укажите возраст посетителей через пробел: ').split()))
+sum_ticket = []
+for i in ages:
+    if i < 18:
+        sum_ticket.append(0)
+    elif 18 <= i < 25:
+        sum_ticket.append(990)
+    else:
+        sum_ticket.append(1390)
+if tickets > 3:
+    sum_disc = round(sum(sum_ticket) - (sum(sum_ticket) * 0.1))
+    print('Сумма к оплате с учетом скидки: ', sum_disc, 'руб.')
+else:
+    print('Сумма к оплате: ', sum(sum_ticket), 'руб.')
